6a2988aaf17cf9b20d476bbc60465938ed58e87e
Kafelixon <piotrszkafel@gmail.com>
2024-03-31 20:47:07 +0200

    Add modal for AddItem to wishlist

--- src/components/ui/WishlistTable.tsx
+++ src/components/ui/WishlistTable.tsx
@@ -4,18 +4,26 @@ import { addItemToWishlist, fetchWishlistItems } from "@/lib/wishlistHandlers";
 import { useToast } from "@/components/ui/use-toast";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
 import { Loader2 } from "lucide-react";
 
 interface WishlistItemsTableProps {
-  isAddMode: boolean;
-  onCancelAddMode: () => void;
+  canEditWishlist: boolean;
   wishlistId: string;
   userId: string | null;
 }
 
 export default function WishlistItemsTable({
-  isAddMode,
-  onCancelAddMode,
+  canEditWishlist,
   wishlistId,
   userId,
 }: WishlistItemsTableProps) {
@@ -26,14 +34,6 @@ export default function WishlistItemsTable({
   const [link, setLink] = useState("");
   const [userWishlist, setUserWishlist] = useState<WishlistItem[]>([]);
   const [isFetching, setIsFetching] = useState(true);
-
-  useEffect(() => {
-    if (isAddMode) {
-      setProductName("");
-      setAveragePrice("");
-      setLink("");
-    }
-  }, [isAddMode]);
 
   useEffect(() => {
     if (wishlistId) {
@@ -51,12 +51,15 @@ export default function WishlistItemsTable({
             parseFloat(a.price.replace("~", "")) -
             parseFloat(b.price.replace("~", ""))
         )
-        .map((item) => (
-          <div className="glass-fg flex items-center border pl-2.5 pr-5 py-2.5 rounded-lg border-solid border-gray-300">
+        .map((item, index) => (
+          <div
+            key={index}
+            className="glass-fg flex gap-2.5 items-center border pl-2.5 pr-5 py-2.5 rounded-lg border-solid border-gray-300"
+          >
             <img
               src={item.image}
               alt="Product Image"
-              className="size-28 mr-2.5 object-cover bg-white border border-gray-300 rounded-lg"
+              className="size-28 object-contain bg-white border border-gray-300 rounded-lg"
             />
             <div className="flex flex-col">
               <h2 className="font-semibold mt-0 mb-1 mx-0">{item.name}</h2>
@@ -81,6 +84,11 @@ export default function WishlistItemsTable({
       console.error("User ID or Wishlist ID is missing");
       return;
     }
+
+    setProductName("");
+    setAveragePrice("");
+    setLink("");
+
     try {
       setIsAdding(true);
       await addItemToWishlist(userId, wishlistId, {
@@ -103,61 +111,83 @@ export default function WishlistItemsTable({
   };
 
   const cancelAddMode = async () => {
-    if (onCancelAddMode) onCancelAddMode();
     await fetchWishlistItems(wishlistId).then((data) => {
       if (data) setUserWishlist(data);
     });
   };
 
-  const addCard = (
-    <div className="glass-fg flex items-center justify-between border pl-2.5 pr-5 py-2.5 rounded-lg border-solid border-gray-300">
-      <div className="size-28 mr-2.5 bg-gray-300 border border-gray-300 rounded-lg"></div>
-      <div className="flex-grow">
-        <form className="flex flex-col gap-1">
-          <Input
-            type="text"
-            className="h-8"
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
-            placeholder="Product Name"
-          />
-          <Input
-            type="number"
-            className="h-8"
-            value={averagePrice}
-            onChange={(e) => setAveragePrice(e.target.value)}
-            placeholder="Average Price"
-          />
-          <Input
-            type="text"
-            className="h-8"
-            value={link}
-            onChange={(e) => setLink(e.target.value)}
-            placeholder="Link"
-          />
-        </form>
-      </div>
-
-      <div className="flex flex-grow flex-col justify-between min-h-full">
-        <Button color="primary" onClick={() => void cancelAddMode()}>
-          Cancel
-        </Button>
-        <Button color="primary" onClick={() => void addListing()}>
-          {isAdding ? (
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            "Add listing"
-          )}
-        </Button>
-      </div>
-    </div>
-  );
+  function addItem() {
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="glass-fg">
+            Add Item
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add Item</DialogTitle>
+            <DialogDescription>
+              Add a new item to your wishlist. Click add when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            {/* //TODO: Add image upload or link */}
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="productName" className="text-right">
+                Product Name
+              </Label>
+              <Input
+                id="productName"
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="averagePrice" className="text-right">
+                Average Price
+              </Label>
+              <Input
+                id="averagePrice"
+                type="number"
+                value={averagePrice}
+                onChange={(e) => setAveragePrice(e.target.value)}
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="link" className="text-right">
+                Link
+              </Label>
+              <Input
+                id="link"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button color="primary" onClick={() => void cancelAddMode()}>
+              Cancel
+            </Button>
+            <Button color="primary" onClick={() => void addListing()}>
+              {isAdding ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                "Add listing"
+              )}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  }
 
   return (
-    <div
-      className="flex flex-col gap-2.5"
-    >
-      {isAddMode && userId && addCard}
+    <div className="flex flex-col gap-2.5">
+      {canEditWishlist && userId && addItem()}
       {isFetching ? "Loading..." : cards}
     </div>
   );
